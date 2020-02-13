e9ecab5323089fe45da5076e631ca0e6c38bdab5
Jesse <jessefoster33@gmail.com>
2020-02-13 16:05:45 -0700

    Importing project

---
+++ courseFinalGradeCalculator/mainForm.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace courseFinalGradeCalculator
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.quitButton = new System.Windows.Forms.Button();
+            this.resetAllButton = new System.Windows.Forms.Button();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.resetQuizButton = new System.Windows.Forms.Button();
+            this.transferQuizMarkButton = new System.Windows.Forms.Button();
+            this.quizzesLabel = new System.Windows.Forms.Label();
+            this.quizMarkLabel = new System.Windows.Forms.Label();
+            this.quizMarkInputTextbox = new System.Windows.Forms.TextBox();
+            this.midtermMarkLabel = new System.Windows.Forms.Label();
+            this.finalExamLabel = new System.Windows.Forms.Label();
+            this.midtermInputTextbox = new System.Windows.Forms.TextBox();
+            this.finalExamInputTextbox = new System.Windows.Forms.TextBox();
+            this.quizMarksReadOnlyTextbox = new System.Windows.Forms.TextBox();
+            this.dropHighLowQuizMarksCheckbox = new System.Windows.Forms.CheckBox();
+            this.percentageReadOnlyTextbox = new System.Windows.Forms.TextBox();
+            this.letterGradeReadOnlyTextbox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // quitButton
+            // 
+            this.quitButton.Location = new System.Drawing.Point(323, 541);
+            this.quitButton.Name = "quitButton";
+            this.quitButton.Size = new System.Drawing.Size(146, 43);
+            this.quitButton.TabIndex = 0;
+            this.quitButton.Text = "&Quit";
+            this.quitButton.UseVisualStyleBackColor = true;
+            this.quitButton.Click += new System.EventHandler(this.QuitButton_Click);
+            // 
+            // resetAllButton
+            // 
+            this.resetAllButton.Location = new System.Drawing.Point(323, 488);
+            this.resetAllButton.Name = "resetAllButton";
+            this.resetAllButton.Size = new System.Drawing.Size(146, 47);
+            this.resetAllButton.TabIndex = 1;
+            this.resetAllButton.Text = "&Reset All Marks";
+            this.resetAllButton.UseVisualStyleBackColor = true;
+            this.resetAllButton.Click += new System.EventHandler(this.ResetAllButton_Click);
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(323, 439);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(146, 43);
+            this.calculateButton.TabIndex = 2;
+            this.calculateButton.Text = "&Calculate Grade";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
+            // 
+            // resetQuizButton
+            // 
+            this.resetQuizButton.Location = new System.Drawing.Point(17, 162);
+            this.resetQuizButton.Name = "resetQuizButton";
+            this.resetQuizButton.Size = new System.Drawing.Size(290, 41);
+            this.resetQuizButton.TabIndex = 3;
+            this.resetQuizButton.Text = "Reset &Quiz Marks";
+            this.resetQuizButton.UseVisualStyleBackColor = true;
+            this.resetQuizButton.Click += new System.EventHandler(this.ResetQuizButton_Click);
+            // 
+            // transferQuizMarkButton
+            // 
+            this.transferQuizMarkButton.Location = new System.Drawing.Point(218, 76);
+            this.transferQuizMarkButton.Name = "transferQuizMarkButton";
+            this.transferQuizMarkButton.Size = new System.Drawing.Size(89, 52);
+            this.transferQuizMarkButton.TabIndex = 4;
+            this.transferQuizMarkButton.Text = "&Transfer";
+            this.transferQuizMarkButton.UseVisualStyleBackColor = true;
+            this.transferQuizMarkButton.Click += new System.EventHandler(this.TransferQuizMarkButton_Click);
+            // 
+            // quizzesLabel
+            // 
+            this.quizzesLabel.AutoSize = true;
+            this.quizzesLabel.Location = new System.Drawing.Point(237, 44);
+            this.quizzesLabel.Name = "quizzesLabel";
+            this.quizzesLabel.Size = new System.Drawing.Size(70, 20);
+            this.quizzesLabel.TabIndex = 5;
+            this.quizzesLabel.Text = "Quizzes:";
+            // 
+            // quizMarkLabel
+            // 
+            this.quizMarkLabel.AutoSize = true;
+            this.quizMarkLabel.Location = new System.Drawing.Point(18, 79);
+            this.quizMarkLabel.Name = "quizMarkLabel";
+            this.quizMarkLabel.Size = new System.Drawing.Size(84, 20);
+            this.quizMarkLabel.TabIndex = 6;
+            this.quizMarkLabel.Text = "Quiz Mark:";
+            // 
+            // quizMarkInputTextbox
+            // 
+            this.quizMarkInputTextbox.Location = new System.Drawing.Point(115, 76);
+            this.quizMarkInputTextbox.Name = "quizMarkInputTextbox";
+            this.quizMarkInputTextbox.Size = new System.Drawing.Size(70, 26);
+            this.quizMarkInputTextbox.TabIndex = 7;
+            // 
+            // midtermMarkLabel
+            // 
+            this.midtermMarkLabel.AutoSize = true;
+            this.midtermMarkLabel.Location = new System.Drawing.Point(328, 364);
+            this.midtermMarkLabel.Name = "midtermMarkLabel";
+            this.midtermMarkLabel.Size = new System.Drawing.Size(109, 20);
+            this.midtermMarkLabel.TabIndex = 8;
+            this.midtermMarkLabel.Text = "Midterm Mark:";
+            // 
+            // finalExamLabel
+            // 
+            this.finalExamLabel.AutoSize = true;
+            this.finalExamLabel.Location = new System.Drawing.Point(307, 398);
+            this.finalExamLabel.Name = "finalExamLabel";
+            this.finalExamLabel.Size = new System.Drawing.Size(130, 20);
+            this.finalExamLabel.TabIndex = 9;
+            this.finalExamLabel.Text = "Final Exam Mark:";
+            // 
+            // midtermInputTextbox
+            // 
+            this.midtermInputTextbox.Location = new System.Drawing.Point(446, 361);
+            this.midtermInputTextbox.Name = "midtermInputTextbox";
+            this.midtermInputTextbox.Size = new System.Drawing.Size(100, 26);
+            this.midtermInputTextbox.TabIndex = 10;
+            // 
+            // finalExamInputTextbox
+            // 
+            this.finalExamInputTextbox.Location = new System.Drawing.Point(446, 395);
+            this.finalExamInputTextbox.Name = "finalExamInputTextbox";
+            this.finalExamInputTextbox.Size = new System.Drawing.Size(100, 26);
+            this.finalExamInputTextbox.TabIndex = 11;
+            // 
+            // quizMarksReadOnlyTextbox
+            // 
+            this.quizMarksReadOnlyTextbox.Location = new System.Drawing.Point(348, 44);
+            this.quizMarksReadOnlyTextbox.Multiline = true;
+            this.quizMarksReadOnlyTextbox.Name = "quizMarksReadOnlyTextbox";
+            this.quizMarksReadOnlyTextbox.ReadOnly = true;
+            this.quizMarksReadOnlyTextbox.Size = new System.Drawing.Size(257, 265);
+            this.quizMarksReadOnlyTextbox.TabIndex = 12;
+            // 
+            // dropHighLowQuizMarksCheckbox
+            // 
+            this.dropHighLowQuizMarksCheckbox.AutoSize = true;
+            this.dropHighLowQuizMarksCheckbox.Location = new System.Drawing.Point(323, 323);
+            this.dropHighLowQuizMarksCheckbox.Name = "dropHighLowQuizMarksCheckbox";
+            this.dropHighLowQuizMarksCheckbox.Size = new System.Drawing.Size(282, 24);
+            this.dropHighLowQuizMarksCheckbox.TabIndex = 13;
+            this.dropHighLowQuizMarksCheckbox.Text = "&Drop Highest && Lowest Quiz Marks";
+            this.dropHighLowQuizMarksCheckbox.UseVisualStyleBackColor = true;
+            // 
+            // percentageReadOnlyTextbox
+            // 
+            this.percentageReadOnlyTextbox.Location = new System.Drawing.Point(481, 447);
+            this.percentageReadOnlyTextbox.Name = "percentageReadOnlyTextbox";
+            this.percentageReadOnlyTextbox.ReadOnly = true;
+            this.percentageReadOnlyTextbox.Size = new System.Drawing.Size(46, 26);
+            this.percentageReadOnlyTextbox.TabIndex = 14;
+            // 
+            // letterGradeReadOnlyTextbox
+            // 
+            this.letterGradeReadOnlyTextbox.Location = new System.Drawing.Point(554, 447);
+            this.letterGradeReadOnlyTextbox.Name = "letterGradeReadOnlyTextbox";
+            this.letterGradeReadOnlyTextbox.ReadOnly = true;
+            this.letterGradeReadOnlyTextbox.Size = new System.Drawing.Size(51, 26);
+            this.letterGradeReadOnlyTextbox.TabIndex = 15;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(707, 633);
+            this.Controls.Add(this.letterGradeReadOnlyTextbox);
+            this.Controls.Add(this.percentageReadOnlyTextbox);
+            this.Controls.Add(this.dropHighLowQuizMarksCheckbox);
+            this.Controls.Add(this.quizMarksReadOnlyTextbox);
+            this.Controls.Add(this.finalExamInputTextbox);
+            this.Controls.Add(this.midtermInputTextbox);
+            this.Controls.Add(this.finalExamLabel);
+            this.Controls.Add(this.midtermMarkLabel);
+            this.Controls.Add(this.quizMarkInputTextbox);
+            this.Controls.Add(this.quizMarkLabel);
+            this.Controls.Add(this.quizzesLabel);
+            this.Controls.Add(this.transferQuizMarkButton);
+            this.Controls.Add(this.resetQuizButton);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.resetAllButton);
+            this.Controls.Add(this.quitButton);
+            this.Name = "mainForm";
+            this.Text = "Grades Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button quitButton;
+        private System.Windows.Forms.Button resetAllButton;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Button resetQuizButton;
+        private System.Windows.Forms.Button transferQuizMarkButton;
+        private System.Windows.Forms.Label quizzesLabel;
+        private System.Windows.Forms.Label quizMarkLabel;
+        private System.Windows.Forms.TextBox quizMarkInputTextbox;
+        private System.Windows.Forms.Label midtermMarkLabel;
+        private System.Windows.Forms.Label finalExamLabel;
+        private System.Windows.Forms.TextBox midtermInputTextbox;
+        private System.Windows.Forms.TextBox finalExamInputTextbox;
+        private System.Windows.Forms.TextBox quizMarksReadOnlyTextbox;
+        private System.Windows.Forms.CheckBox dropHighLowQuizMarksCheckbox;
+        private System.Windows.Forms.TextBox percentageReadOnlyTextbox;
+        private System.Windows.Forms.TextBox letterGradeReadOnlyTextbox;
+    }
+}
+
---
+++ courseFinalGradeCalculator/mainForm.cs
@@ -0,0 +1,275 @@
+﻿//Assignment 2: Final Grades Calculator
+//
+//Author: Jesse Foster
+// November 18, 2019
+//
+//Purpose: Calculating a student's final grades
+//
+//Description: This application will allow the user to enter a number of quiz marks, as well as a midterm and final exam mark,
+// grade them appropiately, and allow the user to deduct the highest/lowest marks if required. Requires a minimum of one quiz mark,
+// a midterm mark, and a final exam mark to run. Deducting the highest and lowest marks only works if number of quizzes input is
+// five or greater.
+
+//remember to return this from Console Application to Windows Form App
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace courseFinalGradeCalculator
+{
+    public partial class mainForm : Form
+    {
+        //declare your variables here
+        
+        double quizMarksAverage;            //this will represent the total for marks to use in the Calculate Grade button calculations, should equal 
+                                            //the quizzes added together, divided by the number of quizzes, then multiplied by 0.20.
+        int quizTransferCounter = 0;        //this is to track the number of quizzes added to the readonly textbox per click on transfer button
+        string letterGrade;
+        double lowestMark = 100;            // the reason this starts at 100 is because the default value is starting at 0; no values are allowed to go below this
+        double highestMark;
+        double sumOfQuizzes = 0;
+        double midTermMark;
+        double finalExamMark;
+        double totalGrade;
+
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void QuitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+        // when the transferQuizMarkButton is pressed, grab the text in the associated
+        // textbox and convert it to an integer, quizMark. This should appear in the readonly textbox.
+
+        private void TransferQuizMarkButton_Click(object sender, EventArgs e)
+
+        {
+            string lineOfInput;
+            int convertedQuizValue;
+            bool valueIsGood;
+
+            do
+            {
+                lineOfInput = quizMarkInputTextbox.Text;
+                valueIsGood = int.TryParse(lineOfInput, out convertedQuizValue);
+                if (!valueIsGood)
+                {
+                    MessageBox.Show("Please enter a numeric value between 0 and 100 in the Quiz Mark Input Box.");
+                    return;
+                }
+               if (convertedQuizValue < 0 || convertedQuizValue > 100)
+                {
+                    MessageBox.Show("Please enter a numeric value between 0 and 100 in the Quiz Mark Input Box.");
+                    return;
+                }
+            }
+            while (!valueIsGood);
+
+            quizMarksReadOnlyTextbox.AppendText(quizMarkInputTextbox.Text + Environment.NewLine); //this will add a new line for each input
+
+            quizTransferCounter++;                                  //counts number of quizzes added
+            
+            Console.WriteLine($"{quizTransferCounter}");            //check if the clicks are being counted
+
+            sumOfQuizzes = sumOfQuizzes + convertedQuizValue;       //total value of quiz marks are added per input
+
+            Console.WriteLine($"{sumOfQuizzes}");                   //check if sumOfQuizzes is being added on each click
+
+            quizMarkInputTextbox.Clear();                           //clears input box to allow next value to be entered
+
+
+            if (convertedQuizValue > highestMark)                   //track highest mark continuously
+            {
+                highestMark = convertedQuizValue;
+            }
+
+            if (convertedQuizValue < lowestMark)                    //track lowest mark continuously
+            {
+                lowestMark = convertedQuizValue;
+            }
+
+            Console.WriteLine($"Highest Mark:{highestMark}");       //test if highest mark is tracking
+            Console.WriteLine($"Lowest Mark:{lowestMark}");         //test if lowest mark is tracking
+
+        }
+
+        private void ResetQuizButton_Click(object sender, EventArgs e)
+        {
+            quizMarkInputTextbox.Clear();
+            quizMarksReadOnlyTextbox.Clear();
+            percentageReadOnlyTextbox.Clear();
+            letterGradeReadOnlyTextbox.Clear();
+            quizTransferCounter = 0;
+            sumOfQuizzes = 0;
+        }
+
+        private void ResetAllButton_Click(object sender, EventArgs e)
+        {
+            quizMarkInputTextbox.Clear();
+            quizMarksReadOnlyTextbox.Clear();
+            midtermInputTextbox.Clear();
+            finalExamInputTextbox.Clear();
+            percentageReadOnlyTextbox.Clear();
+            letterGradeReadOnlyTextbox.Clear();
+            quizTransferCounter = 0;
+            sumOfQuizzes = 0;
+            dropHighLowQuizMarksCheckbox.Checked = false;
+        }
+
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            string lineOfInput;
+            int convertedMidTermValue;
+            int convertedFinalExamValue;
+            bool valueIsGood;
+
+            if (sumOfQuizzes <= 0)
+            {
+                MessageBox.Show("Please enter a numeric value between 0 and 100 in the Quiz Mark Input box and transfer.");
+                return;
+            }
+
+            do
+            {
+                lineOfInput = midtermInputTextbox.Text;
+                valueIsGood = int.TryParse(lineOfInput, out convertedMidTermValue);
+                if (!valueIsGood)
+                {
+                    MessageBox.Show("Please enter a numeric value between 0 and 100 in the Midterm Mark Input Box.");
+                    return;
+                }
+                if (convertedMidTermValue < 0 || convertedMidTermValue > 100)
+                {
+                    MessageBox.Show("Please enter a numeric value between 0 and 100 in the Midterm Mark Input Box.");
+                    return;
+                }
+            }
+            while (!valueIsGood);
+
+            midTermMark = convertedMidTermValue * 0.30;
+
+            do
+            {
+                lineOfInput = finalExamInputTextbox.Text;
+                valueIsGood = int.TryParse(lineOfInput, out convertedFinalExamValue);
+
+
+
+                if (!valueIsGood)
+                {
+                    MessageBox.Show("Please enter a numeric value between 0 and 100 in the Final Exam Mark Input Box.");
+                    return;
+                }
+                if (convertedFinalExamValue < 0 || convertedFinalExamValue > 100)
+                {
+                    MessageBox.Show("Please enter a numeric value between 0 and 100 in the Final Exam Mark Input Box.");
+                    return;
+                }
+            }
+            while (!valueIsGood);
+
+            finalExamMark = convertedFinalExamValue * 0.50;
+
+
+            if (dropHighLowQuizMarksCheckbox.Checked == true && quizTransferCounter >= 5)
+            {
+                sumOfQuizzes = sumOfQuizzes - highestMark - lowestMark;
+                quizMarksAverage = sumOfQuizzes / (quizTransferCounter - 2) * 0.20; //calculates average minus the highest/lowest marks, including the 2 counter totals (because there are now 2 fewer quizzes to divide by)
+                Console.WriteLine($"{quizMarksAverage}");                           //check if quizMarksAverage is calculated
+                //quizTransferCounter cannot go below 3 after this
+
+            }
+            else if (dropHighLowQuizMarksCheckbox.Checked == true && quizTransferCounter < 5)
+
+            {
+                MessageBox.Show("Please enter at least 5 or more quiz marks to drop highest and lowest grade.");
+                return;
+            }
+
+            //else if (dropHighLowQuizMarksCheckbox.Checked == false)
+            //{
+            //    quizMarksAverage = sumOfQuizzes / quizTransferCounter * 0.20;       //calculates average of total grades and weighs them at 20% value for final grade
+            //    Console.WriteLine($"{quizMarksAverage}");                           //check if quizMarksAverage is calculated  
+
+            //}
+            Console.WriteLine($"{midTermMark}");                                    //check if midTermMark calculates
+            Console.WriteLine($"{finalExamMark}");                                  //check if finalExamMark calculates
+
+            totalGrade = quizMarksAverage + midTermMark + finalExamMark;
+            totalGrade = Math.Round(totalGrade, 0, MidpointRounding.AwayFromZero); //rounds decimal value back to a whole number
+
+            if (totalGrade < 0 || totalGrade > 100)
+            { MessageBox.Show("Final grade cannot be below 0% or above 100%.");
+               return;
+            }
+
+            Console.WriteLine($"{quizTransferCounter}");
+           
+            Console.WriteLine($"{totalGrade}");                                    //check if totalGrade calculates
+
+            percentageReadOnlyTextbox.Text = totalGrade.ToString();                //converts the totalGrade value to string for output
+
+            if (totalGrade >= 95)
+            {
+                letterGrade = "A+";
+            }
+            else if (totalGrade >= 90)
+            {
+                letterGrade = "A";
+            }
+            else if (totalGrade >= 85)
+            {
+                letterGrade = "A-";
+            }
+            else if (totalGrade >= 80)
+            {
+                letterGrade = "B+";
+            }
+            else if (totalGrade >= 75)
+            {
+                letterGrade = "B";
+            }
+            else if (totalGrade >= 70)
+            {
+                letterGrade = "B-";
+            }
+            else if (totalGrade >= 67)
+            {
+                letterGrade = "C+";
+            }
+            else if (totalGrade >= 64)
+            {
+                letterGrade = "C";
+            }
+            else if (totalGrade >= 60)
+            {
+                letterGrade = "C-";
+            }
+            else if (totalGrade >= 55)
+            {
+                letterGrade = "D+";
+            }
+            else if (totalGrade >= 50)
+            {
+                letterGrade = "D";
+            }
+            else if (totalGrade <= 49)
+            {
+                letterGrade = "F";
+            }
+
+            letterGradeReadOnlyTextbox.Text = letterGrade.ToString();
+        }
+    }
+}
